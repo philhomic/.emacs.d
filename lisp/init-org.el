719e4b3e15ceebd7ef0b331e76894f6fc2da60d1
fantaghiro <happy.wangpei@gmail.com>
2018-06-10 06:43:27 +0800

    add org-babel languages

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -4,5 +4,15 @@
 ;; config org-bullets
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))) ;; do [M-x eval-region]
+;; config org-babel languages
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((sh         . t)
+   (js         . t)
+   (emacs-lisp . t)
+   (python     . t)
+   (ruby       . t)
+   (scheme     . t)
+   (css        . t)))
 
 (provide 'init-org)
